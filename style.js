0bedfcf1d334620edb08dd33d83f5351f2feef63
Quý <phungxuanquy24721@gmail.com>
2025-03-18 13:11:01 +0700

    uploads login

--- style.js
+++ style.js
@@ -1,17 +1,31 @@
+// LOGIN
 function checkForm() {
   const name = document.getElementById("name").value;
   const password = document.getElementById("password").value;
   const button = document.querySelector("button");
+  const length = 3;
+  const length1 = 30;
+  const length2 = 6;
+  const username = "quy123";
+  const password1 = 123456;
 
-  if (name === "quý" && password === "quy") {
-    window.location.href = "product.html";
-  } else {
-    button.style.background = "red";
-    setTimeout(function () {
-      if ((button.style.background = "red")) {
-        button.style.background = "rgba(255,255,255,0.2)";
-      }
-    }, 2000);
-    return alert("sai tài khoản hoặc mật khẩu");
+  if (name.length < length || name.length > length1) {
+    document.getElementById("mes1").innerText =
+      "Tài khoản đăng nhập phải nằm trong độ dài từ 3 - 30 ký tự";
   }
+  if (password.length < length2) {
+    document.getElementById("mes2").innerText = "sai mật khẩu";
+  }
+  if (name === username && password === password1) {
+    window.location.href = "product.html";
+  }
+  if (name != username || password != password1) {
+    document.getElementById("mes3").innerText = "Sai tài khoản hoặc mật khẩu!";
+  }
+  if (name == "" || password == "") {
+    button.style.background = "red";
+  }
+  setTimeout(function () {
+    button.style.background = "rgba(255,255,255,0.3)";
+  }, 2000);
 }
